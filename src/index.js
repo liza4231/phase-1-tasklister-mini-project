3998a4f5042e61b7183e2e23d8f9cdd5f0f70352
Elizabeth salazar <liza4231@gmail.com>
2025-12-13 20:18:58 -0600

    1st attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const newTaskDescription = document.getElementById("new-task-description").value;
+    const newTask = document.createElement("li");
+    newTask.textContent = newTaskDescription;
+    taskList.appendChild(newTask);
+    taskForm.reset();
+  });
+}); 
